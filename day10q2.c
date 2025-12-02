cb503c06a8af0258958f3b9f05720dbaedf5a315
sanjamjain <sanjamjain3@gmail.com>
2025-12-02 16:27:48 +0530

    Update day10q2.c

--- day10q2.c
+++ day10q2.c
@@ -1,3 +1,5 @@
+// Write a program to display the day of the week based on a number (1–7) using switch-case.
+
 #include <stdio.h>
 
 int main() {
@@ -34,4 +36,5 @@ int main() {
     }
 
     return 0;
+
 }
