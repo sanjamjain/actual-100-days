ce62ae3f683c6d81cf98cc0dd641900ad25d58d6
sanjamjain <sanjamjain3@gmail.com>
2025-12-02 16:29:33 +0530

    Update day12q1.c

--- day12q1.c
+++ day12q1.c
@@ -1,8 +1,8 @@
 //Write a program to calculate library fine based on late days as follows: 
-First 5 days late: ₹2/day 
-Next 5 days late: ₹4/day 
-Next 20 days days late: ₹6/day 
-More than 30 days: Membership Cancelled.
+//First 5 days late: ₹2/day 
+//Next 5 days late: ₹4/day 
+//Next 20 days days late: ₹6/day 
+//More than 30 days: Membership Cancelled.
 
 #include <stdio.h>
 
@@ -31,3 +31,4 @@ int main() {
     return 0;
 
 }
+
